fa81620dba3e4a87a417987aa3cbd1ade530c5c7
marjan khakian <marjankhakian@yahoo.com>
2020-01-09 00:44:25 +0330

    Solved the firebase error:)

--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -1,4 +1,5 @@
 import * as firebase from "firebase";//so when we add * as we take all of those names exports and we create a new variable for them
+import 'firebase/app';
 // import database from 'firebase/database';
 // import firebase from "firebase";
 // import database from "firebase/database";
@@ -6,16 +7,18 @@ import * as firebase from "firebase";//so when we add * as we take all of those 
 const firebaseConfig = {
     apiKey: process.env.FIREBASE_API_KEY,
     authDomain: process.env.FIREBASE_AUTH_DOMAIN,
-    databaseURL: process.env.FIREBASE_DATABASE-URL,
+    databaseURL: "https://expensifyapp-e2676.firebaseio.com"
+   //  databaseURL: process.env.FIREBASE_DATABASE-URL,
     projectId: process.env.FIREBASE_PROJECT_ID,
     storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
     messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
     appId: process.env.FIREBASE_APP_ID,
-    measurementId: process.env.FIREBASE_MEASUREMENT_ID
+    measurementId: process.env.FIREBASE_MEASUREMENT_ID,
   };
 
 
 firebase.initializeApp(firebaseConfig);
+firebase.analytics();
 
 const database = firebase.database();
 
