9924fe0870c949a1d1d4d26a43430ffd8ce0e284
marjan khakian <marjankhakian@yahoo.com>
2020-01-09 01:13:06 +0330

    Fixed the firebase error

--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -7,7 +7,7 @@ import 'firebase/app';
 const firebaseConfig = {
     apiKey: process.env.FIREBASE_API_KEY,
     authDomain: process.env.FIREBASE_AUTH_DOMAIN,
-    databaseURL: "https://expensifyapp-e2676.firebaseio.com"
+    databaseURL: "https://expensifyapp-e2676.firebaseio.com",
    //  databaseURL: process.env.FIREBASE_DATABASE-URL,
     projectId: process.env.FIREBASE_PROJECT_ID,
     storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
